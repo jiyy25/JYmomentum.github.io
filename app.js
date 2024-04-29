93a290f08fffb3d2d110fe672cb81cc4440a8c3d
jiyy25 <jiyy825@gmail.com>
2024-04-29 20:50:18 +0900

    localStorage에 username-key의 유무에 따른 화면 출력

--- app.js
+++ app.js
@@ -3,15 +3,28 @@ const loginInput = document.querySelector("#loginForm input")
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USER_NAME_KEY = "userName"
 
 function loginSubmit(e){
     e.preventDefault();
     const userName = loginInput.value;
-    greeting.innerText = `Hello! ${userName}!`;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem("userName",userName);
+    
     //로컬스토리지에 value에 저장한 userName을 저장. key-value
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USER_NAME_KEY,userName);
+    helloUserName(userName);
 }
 
-loginForm.addEventListener("submit",loginSubmit);
+const saveUserName = localStorage.getItem(USER_NAME_KEY);
+
+if(saveUserName === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",loginSubmit);
+}else{
+    helloUserName(saveUserName);
+};
+
+function helloUserName(name){
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello! ${name}!`;
+}
