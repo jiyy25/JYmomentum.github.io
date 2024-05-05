bdaa69799182505dc607d89dd1051dbf5a521027
jiyy25 <jiyy825@gmail.com>
2024-05-06 03:17:11 +0900

    이미지 경로 수정

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "
 
 const randomImg = images[Math.floor(Math.random() * images.length)];
 const bgImg = document.createElement("img");
-bgImg.src = `img/${randomImg}`
-bgImg.classList="backImg"
+bgImg.src = `./img/${randomImg}`
+bgImg.classList = "backImg"
 
 document.body.prepend(bgImg);
